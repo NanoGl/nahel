18c7715b833a343b586cb641ca606c1a88ae0268
NanoGl <orgonlan@outlook.com>
2026-03-21 14:56:59 -0600

    Primera interfaz de admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+    ]
+]">
+    <h1>Hola</h1>
+</x-admin-layout>
---
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -0,0 +1,40 @@
+@php
+    $links = [
+        [
+            'icon' => 'fa-solid fa-chart-column',
+            'name' => 'Dashboard',
+            'route' => route('admin.dashboard'),
+            'active' => request()->routeIs('admin.dashboard'),
+        ]
+    ];
+@endphp
+
+<aside id="top-bar-sidebar"
+    class="fixed top-0 left-0 z-40 w-64 h-[100dvh] bg-white dark:bg-blue-800 dark:text-white transition-transform -translate-x-full sm:translate-x-0"
+    :class="{
+        'translate-x-0 ease-out': sidebarOpen,
+        '-translate-x-full ease-in': !sidebarOpen
+    }"
+    aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft mt-2">
+        <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-5">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="Flowbite Logo" />
+            <span class="self-center text-lg text-heading font-semibold whitespace-nowrap">Flowbite</span>
+        </a>
+        <ul class="space-y-2 font-medium">
+            @foreach ($links as $link)
+                <li>
+                    <a href="{{ $link['route'] }}"
+                        class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group rounded-lg {{ $link['active'] ? 'bg-gray-100 dark:bg-blue-700' : '' }}">
+                        <span class="inline-flex w-6 h-6 justify-center items-center">
+                            <i class="{{ $link['icon'] }}"></i>
+                        </span>
+                        <span class="ms-3">
+                            {{ $link['name'] }}
+                        </span>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</aside>
