b37fd08c5b6c4e47c24602b124d10e9d109deadc
NanoGl <orgonlan@outlook.com>
2026-05-27 18:13:51 -0600

    Distribuidor SLP

--- resources/views/app/distributors.blade.php
+++ resources/views/app/distributors.blade.php
@@ -1,3 +1,31 @@
 <x-store-layout>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1893342.8414025716!2d-103.27599839550554!3d22.052072740709637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842aa1ffacd75bad%3A0x19cc0a9c3e489476!2sGrupo%20Empresarial%20Nahel!5e0!3m2!1ses-419!2smx!4v1777336313693!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="my-8 mx-14">
+        <div class="flex flex-col gap-2">
+            <h1 class="text-3xl font-bold">
+                Distribuidores
+            </h1>
+            <div class="bg-[var(--variable-primary)] h-2 w-24"></div>
+        </div>
+        <div class="mt-10 block lg:flex">
+            <iframe class="max-w-full"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1893342.8414025716!2d-103.27599839550554!3d22.052072740709637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842aa1ffacd75bad%3A0x19cc0a9c3e489476!2sGrupo%20Empresarial%20Nahel!5e0!3m2!1ses-419!2smx!4v1777336313693!5m2!1ses-419!2smx"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="mx-auto p-2">
+                <div>
+                    <h1 class="font-bold text-lg">
+                        San Luis Potosí
+                    </h1>
+                    <div class="text-md">
+                        <p>
+                            Dirección: Centro, Ignacio Allende 615, Centro Historico, 34208 San Luis Potosí, S.L.P.
+                        </p>
+                        <p>
+                            Tel: +52 444 812 1096
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-store-layout>
